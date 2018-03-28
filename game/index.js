dacd28a0cf6e20f0efac24909bb56c0238c56543
Alona Varshal <thirdwave72@gmail.com>
2018-03-28 15:04:02 -0500

    Create bad function

--- game/index.js
+++ game/index.js
@@ -24,62 +24,77 @@ export const streak = (board, firstCoords, ...coords) => {
 
 export const winner = board => {
   const row1 = streak(board, [0, 0], [0, 1], [0, 2]);
-  if (row1) return row1
+  if (row1) return row1;
 
   const row2 = streak(board, [1, 0], [1, 1], [1, 2]);
-  if (row2) return row2  
+  if (row2) return row2;
 
   const row3 = streak(board, [2, 0], [2, 1], [2, 2]);
-  if (row3) return row3
-  
+  if (row3) return row3;
+
   const col1 = streak(board, [0, 0], [1, 0], [2, 0]);
-  if (col1) return col1
-  
+  if (col1) return col1;
+
   const col2 = streak(board, [0, 1], [1, 1], [2, 1]);
-  if (col2) return col2
-  
+  if (col2) return col2;
+
   const col3 = streak(board, [0, 2], [1, 2], [2, 2]);
-  if (col3) return col3
-  
+  if (col3) return col3;
+
   const diagDown = streak(board, [0, 0], [1, 1], [2, 2]);
-  if (diagDown) return diagDown
-  
+  if (diagDown) return diagDown;
+
   const diagUp = streak(board, [2, 0], [1, 1], [0, 2]);
-  if (diagUp) return diagUp
-  
+  if (diagUp) return diagUp;
 };
 
-
 //Alternates turn from X and O and returns X or O
-const turnReducer = (turn = 'X', action) => {
-  if(action.type === MOVE) {
-    if (turn === 'X') {
-      turn = 'O'
+const turnReducer = (turn = "X", action) => {
+  if (action.type === MOVE) {
+    if (turn === "X") {
+      turn = "O";
     } else {
-      turn = 'X'
+      turn = "X";
     }
   }
-  return turn
-}
+  return turn;
+};
 
 const boardReducer = (board = Map(), action) => {
-  if (action.type === MOVE)
-    board = board.setIn(action.position, action.turn)
-  return board
-}
+  if (action.type === MOVE) board = board.setIn(action.position, action.turn);
+  return board;
+};
 
-const combinedReducer = (state = {}, action) => {
-  const nextBoard = boardReducer(state.board, action)
-  const winnerState = winner(nextBoard)
+const bad = (state, action) => {
+  if (action.type === MOVE) {
+    const newPosition = action.position;
+    console.log(newPosition);
+    console.log(state.board);
+    const currentChar = state.board.getIn(newPosition);
+    console.log(currentChar);
+    if (currentChar !== "_") {
+      return "This position is already taken or invalid.";
+    }
+  }
+  return null;
+};
 
+const combinedReducer = (state = initialState, action) => {
+  const error = bad(state, action);
+  if (error) {
+    console.log(error);
+    return state /* Object.assign({}, state, error) */;
+  }
+  const nextBoard = boardReducer(state.board, action);
+  const winnerState = winner(nextBoard);
   return {
     board: nextBoard,
     turn: turnReducer(state.turn, action),
     winner: winnerState
-  }
-}
+  };
+};
 
-export default combinedReducer
+export default combinedReducer;
 
 // export function reducer(state = { board: Map(), turn: "X" }, action) {
 //   // TODO
