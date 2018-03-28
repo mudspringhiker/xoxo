51a88c5966d5ee88ff733f58520e6ebb2e67ccd2
Blake Zeiger <blakezeiger@gmail.com>
2018-03-28 12:07:55 -0400

    updates

--- game/index.js
+++ game/index.js
@@ -1,6 +1,24 @@
 import {Map} from 'immutable'
 
-export default function reducer(state, action) {
+const initialState = {
+  board: Map(),
+  turn: "X"
+}
+
+const MOVE = 'move'
+//                                                         [1,0]
+export const move =(turn, position) => ({ type: MOVE, turn, position })
+
+export default function reducer(state = { board: Map(), turn: "X" }, action) {
   // TODO
+  if (action.type === MOVE && action.turn === 'X') {
+    //set turn to O and write position to board
+    return { board: state.board.setIn(action.position, action.turn), turn: 'O' }
+
+  } else if (action.type === MOVE && action.turn === 'O') {
+    //set turn to X and write position to board
+    return { board: state.board.setIn(action.position, action.turn), turn: 'X'}
+
+  }
   return state
 }
